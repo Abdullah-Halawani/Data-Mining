6c5bc314c1dd02a07cf860806cba139b72faf904
Abdullah-Halawani <78023797+Abdullah-Halawani@users.noreply.github.com>
2021-01-29 19:53:40 +0300

    Add files via upload

---
+++ Lab01_Abdullah Halawani 3410933.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print ("Hello World")
+
+
+# <p> This is my <b> first Python code </b>
+hello world this is my first python code
+# https://www.taibahu.edu.sa
+
+# $$c = \sqrt{a^2 + b^2}$$
+
+# $$e^{i\pi} + i = 0$$
+
+# $$r'F(k) = \int_{-\infty}^{\infty} f(x) e^{2\pi i k} dx ' $$
+
+# $$e^x=\sum_{i=0}^\infty \frac{1}{i!}x^i$$
+
+# $$\begin{pmatrix}
+# 1 & a_1 & a_1^2 & \cdots & a_1^n \\
+# 1 & a_2 & a_2^2 & \cdots & a_2^n \\
+# \vdots & \vdots & \vdots & \ddots & \vdots \\
+# 1 & a_m & a_m^2 & \cdots & a_m^n
+# \end{pmatrix}$$
+
+# $$f(x) = a_{0}+ \sum_{n=1}^\infty (a_{n} cos\frac{n\pi x}{L} + b_{n} sin\frac{n\pi x}{L}) $$
+
+# $$ (x+a)^n = \sum_{k=0}^n \begin{pmatrix} n \\ k \end{pmatrix} x^k a^{n-k} $$
+
+# 
